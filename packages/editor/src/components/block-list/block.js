a3e2a838ae0327295079dbb42441c209ede9034d
Anton Timmermans <email@atimmer.com>
2018-09-11 16:44:09 +0200

    Implement annotations

--- packages/editor/src/components/block-list/block.js
+++ packages/editor/src/components/block-list/block.js
@@ -388,6 +388,7 @@ export class BlockListBlock extends Component {
 			isPreviousBlockADefaultEmptyBlock,
 			isParentOfSelectedBlock,
 			isDraggable,
+			annotations,
 		} = this.props;
 		const isHovered = this.state.isHovered && ! isMultiSelecting;
 		const { name: blockName, isValid } = block;
@@ -417,6 +418,10 @@ export class BlockListBlock extends Component {
 		const shouldShowInsertionPoint = ( isPartOfMultiSelection && isFirstMultiSelected ) || ! isPartOfMultiSelection;
 		const canShowInBetweenInserter = ! isEmptyDefaultBlock && ! isPreviousBlockADefaultEmptyBlock;
 
+		const annotationsClassNames = annotations.map( ( annotation ) => {
+			return 'is-annotated-by-' + annotation.source;
+		} );
+
 		// The wp-block className is important for editor styles.
 		// Generate the wrapper class names handling the different states of the block.
 		const wrapperClassName = classnames( 'wp-block editor-block-list__block', {
@@ -424,12 +429,13 @@ export class BlockListBlock extends Component {
 			'is-selected': shouldAppearSelected,
 			'is-multi-selected': isPartOfMultiSelection,
 			'is-hovered': shouldAppearHovered,
+			'is-annotated': annotations.length !== 0,
 			'is-reusable': isReusableBlock( blockType ),
 			'is-dragging': dragging,
 			'is-typing': isTypingWithinBlock,
 			'is-focused': isFocusMode && ( isSelected || isParentOfSelectedBlock ),
 			'is-focus-mode': isFocusMode,
-		} );
+		}, annotationsClassNames );
 
 		const { onReplace } = this.props;
 
@@ -601,6 +607,7 @@ const applyWithSelect = withSelect( ( select, { clientId, rootClientId, isLargeV
 		getEditorSettings,
 		hasSelectedInnerBlock,
 		getTemplateLock,
+		getAnnotationsForBlock,
 	} = select( 'core/editor' );
 	const isSelected = isBlockSelected( clientId );
 	const { hasFixedToolbar, focusMode } = getEditorSettings();
@@ -634,6 +641,7 @@ const applyWithSelect = withSelect( ( select, { clientId, rootClientId, isLargeV
 		block,
 		isSelected,
 		isParentOfSelectedBlock,
+		annotations: getAnnotationsForBlock( clientId ),
 	};
 } );
 
