19c740aa39239869ea3f13e71f002acc17acca71
Anton Timmermans <email@atimmer.com>
2018-10-31 16:20:11 +0100

    Only re-render one block when adding annotation
    Do this by optimizing the annotations state to keep track of which
    annotations belongs to which block. This information can then be used
    in the selector to return the same reference if the annotations for a
    block haven't changed.

--- packages/editor/src/store/selectors.js
+++ packages/editor/src/store/selectors.js
@@ -2091,23 +2091,23 @@ export function isPublishSidebarEnabled( state ) {
  */
 export const getAnnotationsForBlock = createSelector(
 	( state, clientId ) => {
-		return state.annotations.filter( ( annotation ) => {
+		return state.annotations.all.filter( ( annotation ) => {
 			return !! annotation.isBlockAnnotation && annotation.block === clientId;
 		} );
 	},
-	( state ) => [
-		state.annotations,
+	( state, clientId ) => [
+		state.annotations.byBlockId[ clientId ],
 	]
 );
 
 export const getAnnotationsForRichText = createSelector(
 	( state, clientId ) => {
-		return state.annotations.filter( ( annotation ) => {
+		return state.annotations.all.filter( ( annotation ) => {
 			return ! annotation.isBlockAnnotation && annotation.block === clientId;
 		} );
 	},
-	( state ) => [
-		state.annotations,
+	( state, clientId ) => [
+		state.annotations.byBlockId[ clientId ],
 	]
 );
 
